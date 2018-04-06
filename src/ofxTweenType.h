8e359d7206d02bf4329468a124922be284c79e35
nariakiiwatani <nariakiiwatani@annolab.com>
2018-04-06 15:57:11 +0900

    avoid possible crashing

--- src/ofxTweenType.h
+++ src/ofxTweenType.h
@@ -19,6 +19,12 @@ inline Type ofxTweenType(float k, float k0, float k1, const Type &v0, const Type
 template<typename Type, typename Interpolator=typename ofx::tweentype::interpolate::Tipical<Type>::type>
 inline Type ofxTweenType(float k, float k0, float k1, const Type &v0, const Type &v1, std::vector<ofEaseFunction> ease)
 {
+	if(ease.size() < Interpolator::dim()) {
+		if(ease.empty()) {
+			ease.emplace_back(OF_EASE_LINEAR_INOUT);
+		}
+		ease.insert(end(ease), Interpolator::dim()-ease.size(), ease.back());
+	}
 	return Interpolator::getInterpolated(k,k0,k1,v0,v1,ease);
 }
 
